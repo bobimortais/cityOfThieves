e72c3424434ec6a65db85232144606f5e65bbbf5
bobimortais <robsonzacarias@hotmail.com>
2020-05-21 14:08:09 -0300

    Remove pages

--- pages/addHistoryToHtmlIndex.py
+++
@@ -1,9 +0,0 @@
-indexUpdated = open('indexUpdated.html', 'r+')
-file = open('compiledHistories.txt', 'r')
-lines = file.readlines()
-breakLine = '\n'
-lines.insert(0, breakLine)
-indexUpdated.seek(164)
-indexUpdated.writelines(lines)
-file.close()	
-indexUpdated.close()
